16e08816e464cab279b47e22bce6df4cc4d8123f
evan <evan.tirta@hotmail.com>
2017-11-12 13:45:11 +0700

    gui + listener ready

--- src/main/java/realsimulator/MainViewer.java
+++ src/main/java/realsimulator/MainViewer.java
@@ -1,7 +1,15 @@
 package realsimulator;
 
 import javax.swing.*;
+import javax.swing.text.DateFormatter;
+import javax.swing.text.DefaultFormatterFactory;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Hashtable;
 
 /**
@@ -12,6 +20,10 @@ public class MainViewer {
     final static boolean shouldFill = true;
     final static boolean shouldWeightX = true;
     final static boolean RIGHT_TO_LEFT = false;
+
+    static final int temp_MIN = -10;
+    static final int temp_MAX = 40;
+    static final int temp_INIT = 25;
     
     public static void createAndShowGUI(){
         //Create and set up the window.
@@ -47,22 +59,14 @@ public class MainViewer {
         OTlabel.setFont(new Font("Arial", Font.PLAIN, 14));
         pane.add(OTlabel,c);
 
-        JSlider slider = new JSlider();
+        JSlider slider = new JSlider(JSlider.HORIZONTAL, temp_MIN, temp_MAX, temp_INIT);
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 0;
         c.weightx = 1;
         c.gridy = 1;
-        slider.setMajorTickSpacing(20);
+        slider.setMajorTickSpacing(10);
         slider.setPaintTicks(true);
         slider.setPaintLabels(true);
-        Hashtable position = new Hashtable();
-        position.put(0, new JLabel("-10"));
-        position.put(20, new JLabel("0"));
-        position.put(40, new JLabel("10"));
-        position.put(60, new JLabel("20"));
-        position.put(80, new JLabel("30"));
-        position.put(100, new JLabel("40"));
-        slider.setLabelTable(position);
         pane.add(slider, c);
 
         JLabel WSlabel =new JLabel("Wind Speed (KM/H)");
@@ -91,6 +95,51 @@ public class MainViewer {
         slider1.setLabelTable(position1);
         pane.add(slider1, c);
 
+//        JFormattedTextField timeInput = new JFormattedTextField();
+//        timeInput.setFormatterFactory(new DefaultFormatterFactory(new DateFormatter(DateFormat.getTimeInstance())));
+//        c.fill = GridBagConstraints.HORIZONTAL;
+//        c.gridx = 0;
+//        c.weightx = 1;
+//        c.gridy = 4;
+//        pane.add(timeInput, c);
 
+        Date date = new Date();
+        JSpinner timeSpinner = new JSpinner( new SpinnerDateModel() );
+        JSpinner.DateEditor timeEditor = new JSpinner.DateEditor(timeSpinner, "HH:mm");
+        timeSpinner.setEditor(timeEditor);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 1;
+        c.gridx = 0;
+        c.gridy = 4;
+        pane.add(timeSpinner, c);
+
+
+        JButton SP = new JButton("Set Parameter");
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 1;
+        c.gridx = 0;
+        c.gridy = 5;
+        pane.add(SP, c);
+
+
+        class calculateListener1 implements ActionListener{
+            public void actionPerformed(ActionEvent event){
+                int temp = slider.getValue();
+                int wind = slider1.getValue();
+                Object timetemp = timeSpinner.getValue();
+                SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+                String time = format.format(date);
+
+
+                System.out.println(temp);
+                System.out.println(wind);
+                System.out.println(time);
+
+
+            }
+        }
+
+        ActionListener listener1 = new calculateListener1();
+        SP.addActionListener(listener1);
     }
 }
